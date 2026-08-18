26977e26f1882b14f509566925dd56c1d667e733
Sanket Pandit <sanket.pandit@amd.com>
2026-08-18 20:31:14 +0000

    perf(gemm): tune Kimi latent projection by batch
    Use fused Gluon for M=1/2 and the composed MFMA path from M=3 through M=16.
    
    Signed-off-by: Sanket Pandit <sanket.pandit@amd.com>
    (cherry picked from commit 3a351c31d7d2ce1b9f598d163458aac63514707b)

--- tokenspeed-kernel/python/tokenspeed_kernel/ops/gemm/kimi3.py
+++ tokenspeed-kernel/python/tokenspeed_kernel/ops/gemm/kimi3.py
@@ -15,7 +15,6 @@ from dataclasses import dataclass
 from functools import lru_cache
 
 import torch
-
 from tokenspeed_kernel._triton import libdevice, tl, triton
 from tokenspeed_kernel.platform import Platform
 
